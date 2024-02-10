42be80c257052fae86dbf1ce58f1ff0424cb99a6
krisdiallo <71157512+krisdiallo@users.noreply.github.com>
2024-02-10 10:05:54 -0500

    retrieve crypto links from cmc coin list and get historical data for each crypto

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ Created on Sat Sep 23 19:05:35 2017
 @author: SuperKogito
 """
 from gui import MainWindow
-
+from scrape import Scraper
 
 def main():
-    MainWindow('Scraper')
-
+    #MainWindow('Scraper')
+    scraper = Scraper()
+    list_of_cryptos = scraper.get_cryptos(1)
+    for coin in list_of_cryptos:
+        data = scraper.process(coin)
+        scraper.write_to_csv(data, coin.split("/")[-2].upper())
 if __name__ == '__main__':
     main()
--- scrape.py
+++ scrape.py
@@ -5,17 +5,89 @@ Created on Sat Apr 21 19:37:45 2018
 @author: kogito
 """
 import csv
+import re
+import string
 import requests
 from bs4 import BeautifulSoup
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime as dt
+from datetime import timedelta
 
+currency_list_url = "https://coinmarketcap.com"
 
 class Scraper():
+      
+    
     @classmethod
-    def process(self, url):
-        r = requests.get(url)
-        soup = BeautifulSoup(r.text, 'lxml')
+    def get_cryptos(self, pages):
+        crypto_list = []
+        for p in range(pages):
+            page_url = currency_list_url + f"/?page={p+1}"
+            r = requests.get(page_url)
+            soup = BeautifulSoup(r.text, 'lxml')
+            table = soup.find('table')
+            for row in table.find_all('tr'):
+                crypto_name = row.find('a', {"class": "cmc-link"})
+                if crypto_name:
+                    crypto_name = crypto_name.attrs.get("href")
+                    crypto_list.append(crypto_name)
+        return crypto_list
+            
+            
+                
+    @classmethod
+    def process(self, coin_url, start_date=dt.now() - timedelta(365)):
+        coin = coin_url.split("/")[-2].upper()
+        print(start_date)
+        url = currency_list_url + coin_url + "historical-data"
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument('--window-size=1200,1100')
+        browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        browser.get(url)
+        while True:
+            try:
+                WebDriverWait(browser, 3).until(
+                    EC.presence_of_element_located((By.TAG_NAME, 'body'))
+                )
+                browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                xpath_expression = "//button[contains(@class, 'sc-2861d03b-0') and text()='Load More']"
+                load_more_button = WebDriverWait(browser, 1).until(
+                    EC.visibility_of_element_located((By.XPATH, xpath_expression))
+                )
+                if load_more_button.is_displayed():
+                    load_more_button.click()
+                else:
+                    break
+
+                html = browser.page_source
+                soup = BeautifulSoup(html, features="html.parser")
+                table = soup.find('table')
+                rows = table.find_all('tr')
+                last_row = rows[-1]
+                last_date = last_row.findChildren('td')[0].string
+
+                if dt.strptime(last_date, "%b %d, %Y") <= start_date:
+                    print(f"{last_date} is earlier than {start_date}")
+                    break
+
+            except EC.NoSuchElementException:
+                break
+            except Exception as e:
+                print("An error occurred:", e)
+                break
+        print(f"finishing execution of {coin}")     
         data = []
-        table = soup.find('table', id='')
+        html = browser.page_source
+        soup = BeautifulSoup(html, features="html.parser")
+        table = soup.find('table')
         for row in table.find_all('tr'):
             cells = row.findChildren('td')
             values = []
@@ -32,18 +104,18 @@ class Scraper():
                 MarketCap = values[6]
             except IndexError:
                 continue
-            data.append([Date, Open, High, Low, Close, Volume, MarketCap])
-        # Print data
-        for item in data:
-            print(item)
+            data.append([Date, coin, Open, High, Low, Close, Volume, MarketCap])
+        # for item in data:
+        #     print(item)
+        browser.quit()
         return data
 
     @classmethod
-    def write_to_csv(self, data):
-        f = open('ScrapedData.csv', 'w')
+    def write_to_csv(self, data, crypto):
+        f = open(f'{crypto}_1d.csv', 'w')
         with f:
-            writer = csv.writer(f)
-            writer.writerow(['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'MarketCap'])
+            writer = csv.writer(f, quoting=csv.QUOTE_NONE, escapechar='\\')
+            writer.writerow(['Date', "Coin", 'Open', 'High', 'Low', 'Close', 'Volume', 'MarketCap'])
 
             for row in data:
                 writer.writerow(row)
